6e226f42b9e9ad5d84c5fb2b70651e2111220b09
Marlys Alvarado <sylram@icloud.com>
2024-03-08 20:46:47 -0500

    accordion component

---
+++ src/AccordionComponent.js
@@ -0,0 +1,24 @@
+import { Accordion, AccordionBody, AccordionHeader, AccordionItem } from "react-bootstrap";
+// import Accordion from "react-bootstrap/Accordion";
+import "bootstrap/dist/css/bootstrap.min.css";
+import faqs from "./faqs.json"
+
+
+export default function AccordionComponent() {
+
+    return (
+
+        <div className="content">
+            <div className="header">FAQs</div>
+            <Accordion flush>
+                {faqs.questions.map((element, index) => (
+                    <Accordion.Item eventKey= {index} >
+                        <Accordion.Header className="acc-header">{element.question}</Accordion.Header>
+                        <Accordion.Body className="acc-body">{element.answer}</Accordion.Body>
+                    </Accordion.Item>
+                ))}
+            </Accordion>
+        </div>
+    )
+
+}
