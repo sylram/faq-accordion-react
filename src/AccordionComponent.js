88e46d0580bb84849bb2d0d7a6a8d1a11105fcc7
Marlys Alvarado <sylram@icloud.com>
2024-03-12 14:00:19 -0400

    responsive accordion

--- src/AccordionComponent.js
+++ src/AccordionComponent.js
@@ -2,20 +2,21 @@ import { Accordion, AccordionBody, AccordionHeader, AccordionItem } from "react-
 // import Accordion from "react-bootstrap/Accordion";
 import "bootstrap/dist/css/bootstrap.min.css";
 import faqs from "./faqs.json"
-
+import star from "./images/icon-star.svg"
 
 export default function AccordionComponent() {
 
     return (
 
         <div className="content">
-            <div className="header">FAQs</div>
+            <div className="header"><span className="icon-star"><img src={star}/></span>FAQs</div>
             <Accordion flush>
                 {faqs.questions.map((element, index) => (
                     <Accordion.Item eventKey= {index} >
                         <Accordion.Header className="acc-header">{element.question}</Accordion.Header>
                         <Accordion.Body className="acc-body">{element.answer}</Accordion.Body>
                     </Accordion.Item>
+                    
                 ))}
             </Accordion>
         </div>
